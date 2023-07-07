021b32032ad7538892d5c7b834b39b35e160ca7a
CeedeBee <111492957+CeedeBee@users.noreply.github.com>
2023-07-07 18:24:35 +0100

    Update hamming.lua

--- hamming.lua
+++ hamming.lua
@@ -1,20 +1,17 @@
-local Hamming = {}
-    
-function Hamming.compute(s1,s2)
-        
-if #s1 ~= #s2 then           
+local Hamming = {}  
+
+function Hamming.compute(s1,s2)        
+  if #s1 ~= #s2 then           
     return -1
-end -- if strings different lengths
+  end -- if strings different lengths
     
-local distance = 0        
-for i = 1, #s1 do
+  local distance = 0        
+  for i = 1, #s1 do
     if s1:byte(i) ~= s2:byte (i) then
        distance = distance + 1  
     end           
-end 
-    
-return distance           
-                       
+  end 
+  return distance                                  
 end
 
 return Hamming
